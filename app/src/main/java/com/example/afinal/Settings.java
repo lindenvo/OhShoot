63370d44f4cd40749fa6d0e07a92895099bce477
lindenvo <lindenvo@bu.edu>
2019-05-01 18:31:04 -0400

    Linden Vo 3AM

--- app/src/main/java/com/example/afinal/Settings.java
+++ app/src/main/java/com/example/afinal/Settings.java
@@ -6,19 +6,18 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
 
 public class Settings extends AppCompatActivity {
     private Button finished;
-
-
     private TextView settings;
-
     private SeekBar volumeSeekbar = null;
     private AudioManager audioManager = null;
-
+    boolean checked = true;
+    private CheckBox soundEnable;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,17 +25,14 @@ public class Settings extends AppCompatActivity {
 
         setVolumeControlStream(AudioManager.STREAM_MUSIC);
         initControls();
+        soundEnable = findViewById(R.id.sound);
         settings = findViewById(R.id.settings);
 
         //Initializes button to the parameters in result.xml
         finished = findViewById(R.id.backbutton);
 
-
     }
 
-    public void goBack(View v) {
-        finish();
-    }
 
     public void initControls() {
         volumeSeekbar = findViewById(R.id.volumecontrol);
@@ -45,7 +41,6 @@ public class Settings extends AppCompatActivity {
                 .getStreamMaxVolume(AudioManager.STREAM_MUSIC));
         volumeSeekbar.setProgress(audioManager
                 .getStreamVolume(AudioManager.STREAM_MUSIC));
-
 
         volumeSeekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
@@ -67,8 +62,46 @@ public class Settings extends AppCompatActivity {
         });
     }
 
-    public void mutemusic(View view) {
-        audioManager.adjustStreamVolume(AudioManager.STREAM_MUSIC, AudioManager.ADJUST_TOGGLE_MUTE,
-                0);
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.sound:
+                if (checked) {
+                    checked = false;
+                    audioManager.adjustStreamVolume(AudioManager.STREAM_MUSIC,
+                            AudioManager.ADJUST_MUTE, 0);
+                    volumeSeekbar.setEnabled(false);
+
+                } else {
+
+                    checked = true;
+                    audioManager.adjustStreamVolume(AudioManager.STREAM_MUSIC,
+                            AudioManager.ADJUST_UNMUTE, 0);
+                    volumeSeekbar.setEnabled(true);
+                    volumeSeekbar.setMax(audioManager
+                            .getStreamMaxVolume(AudioManager.STREAM_MUSIC));
+                    volumeSeekbar.setProgress(audioManager
+                            .getStreamVolume(AudioManager.STREAM_MUSIC));
+                }
+                break;
+        }
     }
+    public void goBack(View v) {
+        finish();
+    }
+
+   /* public void mutemusic(){
+        if (music.isChecked() == true) {
+            audioManager.adjustStreamVolume(AudioManager.STREAM_MUSIC,
+                    AudioManager.ADJUST_UNMUTE, 0);
+            volumeSeekbar.setMax(audioManager
+                    .getStreamMaxVolume(AudioManager.STREAM_MUSIC));
+            volumeSeekbar.setProgress(audioManager
+                    .getStreamVolume(AudioManager.STREAM_MUSIC));
+        }
+        else{
+            audioManager.adjustStreamVolume(AudioManager.STREAM_MUSIC,
+                    AudioManager.ADJUST_MUTE, 0);
+            volumeSeekbar.setEnabled(false);
+        }
+    }*/
 }
